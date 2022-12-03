221c532ed8f4d0af6410dace6cf04e381015486a
Dhruv agarwal <dhruv@Dhruvs-MacBook-Pro.local>
2022-12-03 09:37:52 +0530

    Contracts added

---
+++ Contracts/profileManager.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+/*
+Profile Manager , Issuer of  .swift alias , or other aliases can be stored in the profile
+- Issue profiles , and store user details 
+- fetch and verify the user details , 
+- Allow only one profile via World Coin 
+- Deploy an Biconomy Smart Contract Wallet 
+*/
