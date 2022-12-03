7a55fc4f0e92070c2bd99ba94c93320926b22984
anishjain <helloanishjain@gmail.com>
2022-12-03 15:06:06 +0530

    add text in app

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -12,9 +12,8 @@ export default function Home() {
       </Head>
 
       <div>
-        
+        <p>HOMEEEEEEEEEE</p>
       </div>
-
     </div>
-  )
+  );
 }
